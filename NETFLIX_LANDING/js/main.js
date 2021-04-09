b35239e439c912af592532ff15a1874d55866676
Fernando Esquirio Torres <esquirio@Fernandos-MacBook-Air.local>
2021-04-09 10:49:30 +0200

    Netflix Layout-Fix problems 2

--- NETFLIX_LANDING/js/main.js
+++ NETFLIX_LANDING/js/main.js
@@ -3,32 +3,25 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
-	// Remove all show and border classes
-	removeBorder();
-	removeShow();
-	// Add border to current tab item
-	this.classList.add('tab-border');
-	// Grab content item from DOM
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
-	// Add show class
-	tabContentItem.classList.add('show');
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class
+    tabContentItem.classList.add('show');
 }
 
-// Remove bottom borders from all tab items
 function removeBorder() {
-	tabItems.forEach(item => {
-		item.classList.remove('tab-border');
-	});
+    tabItems.forEach(item => item.classList.remove('tab-border'));
 }
 
-// Remove show class from all content items
 function removeShow() {
-	tabContentItems.forEach(item => {
-		item.classList.remove('show');
-	});
+    tabContentItems.forEach(item => item.classList.remove('show'));
 }
 
-// Listen for tab item click
+// Listen for tab click
 tabItems.forEach(item => {
-	item.addEventListener('click', selectItem);
+    item.addEventListener('click', selectItem);
 });
