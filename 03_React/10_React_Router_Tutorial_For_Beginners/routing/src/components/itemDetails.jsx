dcd4c4b91694f88879d4ccdcbde920f242337138
Fernando Esquirio Torres <esquirio@gmail.com>
2021-06-17 22:20:52 +0200

    React_Router_Tutorial_For_Beginners

--- 03_React/10_React_Router_Tutorial_For_Beginners/routing/src/components/itemDetails.jsx
+++ 03_React/10_React_Router_Tutorial_For_Beginners/routing/src/components/itemDetails.jsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
 function ItemDetails({ match }) {
-    const [item, setItem] = useState({});
+    const [item, setItem] = useState({
+        data: {
+            item: {
+                name: null,
+                type: null,
+                rarity: null,
+                images: {
+                    icon: null,
+                },
+            },
+        },
+    });
 
     useEffect(() => {
         fetchItem();
@@ -21,32 +32,38 @@ function ItemDetails({ match }) {
     };
 
     // console.log('Teste');
-    // console.log(
-    //     Object.entries(item).length === 0 ? 'Wait' : item.data.item.name
-    // );
+    // console.log(Object.entries(item).length === 0 ? 'Wait' : item);
 
     return (
         <div className="uol">
             <h1>Item</h1>
             <div className="shop-list">
                 <h3>
-                    Name:{' '}
+                    Name: {item.data.item.name}
+                    {/* Name:{' '}
                     {Object.entries(item).length === 0
                         ? 'Loading...'
-                        : item.data.item.name}
+                        : item.data.item.name} */}
                 </h3>
                 <p>
-                    Type:{' '}
+                    Type: {item.data.item.type}
+                    {/* Type:{' '}
                     {Object.entries(item).length === 0
                         ? 'Loading...'
-                        : item.data.item.type}
+                        : item.data.item.type} */}
                 </p>
                 <p>
-                    Rarity:{' '}
+                    Rarity: {item.data.item.rarity}
+                    {/* Rarity:{' '}
                     {Object.entries(item).length === 0
                         ? 'Loading...'
-                        : item.data.item.rarity}
+                        : item.data.item.rarity} */}
                 </p>
+                <img
+                    className="images"
+                    src={item.data.item.images.icon}
+                    alt="Not Found"
+                />
             </div>
         </div>
     );
